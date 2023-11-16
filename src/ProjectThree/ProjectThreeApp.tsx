35385be48faaa15b4112ec5f9ba53e0acb5feca7
aman mulani <mulaniaman0504@gmail.com>
2023-11-16 23:11:55 +0530

    implemented yup package

--- src/ProjectThree/ProjectThreeApp.tsx
+++ src/ProjectThree/ProjectThreeApp.tsx
@@ -1,10 +1,15 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
-
+//Form Validation
+import * as Yup from 'yup'
+const passWordSchema = Yup.object().shape({
+  PasswordLength:Yup.number().min(4,'minimum 4 digit required')
+  .max(16,'maximum 16 digit').required('length is required')
+})
 export default function ProjectThreeApp() {
   return (
     <View>
-      <Text>ProjectThreeApp</Text>
+      <Text>App</Text>
     </View>
   )
 }
