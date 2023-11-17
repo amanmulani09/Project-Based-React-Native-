3b20262e08166d004295d21d3f16c943678c55b2
aman mulani <mulaniaman0504@gmail.com>
2023-11-17 10:54:33 +0530

    implemented password generator logic

--- src/ProjectThree/ProjectThreeApp.tsx
+++ src/ProjectThree/ProjectThreeApp.tsx
@@ -11,15 +11,38 @@ export default function ProjectThreeApp() {
   const [symbols,setSymbols] = useState(false);
 
   const generatePasswordString = function(length:number){
-    //
+    let charactersStr = ''
+    const lowerCaseChars = 'abcdefghijklmnopqrstuvwxyz';
+    const upperCaseChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const numariChars = '1234567890';
+    const specialChars = '!@#$%^&*()_?>+-_'
+
+    charactersStr+= lowerCase ? lowerCaseChars : '';
+    charactersStr+= upperCase ? upperCaseChars : '';
+    charactersStr+= numbers ? numariChars : '';
+    charactersStr+= symbols ? specialChars : '';
+
+    const passwordResult = createPassword(charactersStr,length);
+    setPassword(passwordResult);
+    setIspassGenerated(true);
   }
 
   const createPassword = function(characters:string,length:number){
-//
+    let result = '';
+    for(let i=0; i<length;i++){
+      let randomCharIndex = Math.round(Math.random() * characters.length);
+      result+= characters.charAt(randomCharIndex);
+    }
+    return result;
   }
 
   const resetPassword = ()=>{
-    //
+    setPassword('');
+    setIspassGenerated(false);
+    setLowerCase(false);
+    setUpperCase(false);
+    setNumbers(false);
+    setSymbols(false);
   }
   return (
     <View>
