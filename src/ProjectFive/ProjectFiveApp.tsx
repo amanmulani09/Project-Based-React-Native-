41a188fe558016b78bb148c3f40dab03f2670dcb
aman mulani <mulaniaman0504@gmail.com>
2023-11-24 10:34:20 +0530

    building role the dice app

--- src/ProjectFive/ProjectFiveApp.tsx
+++ src/ProjectFive/ProjectFiveApp.tsx
@@ -1,5 +1,17 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { ImageSourcePropType, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import type { PropsWithChildren } from 'react';
+
+import DiceOne from './assets/One.png'
+import DiceTwo from './assets/Two.png'
+import DiceThree from './assets/Three.png'
+import DiceFour from './assets/Four.png'
+import DiceFive from './assets/Five.png'
+import DiceSix from './assets/Six.png'
+
+type DiceProp = PropsWithChildren<{
+  image:ImageSourcePropType
+}>
 
 export default function ProjectFiveApp() {
   return (
@@ -9,4 +21,29 @@ export default function ProjectFiveApp() {
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#FFF2F2',
+  },
+  diceContainer: {
+    margin: 12,
+  },
+  diceImage: {
+    width: 200,
+    height: 200,
+  },
+  rollDiceBtnText: {
+    paddingVertical: 10,
+    paddingHorizontal: 40,
+    borderWidth: 2,
+    borderRadius: 8,
+    borderColor: '#E5E0FF',
+    fontSize: 16,
+    color: '#8EA7E9',
+    fontWeight: '700',
+    textTransform: 'uppercase',
+  },
+});
