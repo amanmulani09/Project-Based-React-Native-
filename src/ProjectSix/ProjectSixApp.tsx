d67a5e162909a24098e6c71267bb759538f75186
aman mulani <mulaniaman0504@gmail.com>
2023-12-05 11:18:15 +0530

    added auto linking

--- src/ProjectSix/ProjectSixApp.tsx
+++ src/ProjectSix/ProjectSixApp.tsx
@@ -1,4 +1,4 @@
-import { SafeAreaView, StatusBar, StyleSheet, Text, TextInput, View } from 'react-native'
+import { SafeAreaView, StatusBar, StyleSheet, Text, View } from 'react-native'
 import React, { useState } from 'react'
 
 import Snackbar from 'react-native-snackbar';
@@ -41,18 +41,72 @@ const buttonPressed = (targetValue:Currency)=>{
     })
   }
 }
+
   return (
-      <SafeAreaView>
-        <View style={styles.container}>
-        <Text>Hello</Text>
-        </View>
-      </SafeAreaView>
+   <SafeAreaView>
+    <StatusBar />
+    <View>
+      <Text>1</Text>
+    </View>
+   </SafeAreaView>
   )
 }
 
 const styles = StyleSheet.create({
-container:{
-  flex:1,
-  backgroundColor:'red'
-}
+  container: {
+    flex: 1,
+    backgroundColor: '#515151',
+  },
+  topContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'space-evenly',
+  },
+  resultTxt: {
+    fontSize: 32,
+    color: '#000000',
+    fontWeight: '800',
+  },
+  rupee: {
+    marginRight: 8,
+
+    fontSize: 22,
+    color: '#000000',
+    fontWeight: '800',
+  },
+  rupeesContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  inputAmountField: {
+    height: 40,
+    width: 200,
+    padding: 8,
+    borderWidth: 1,
+    borderRadius: 4,
+    backgroundColor: '#FFFFFF',
+  },
+  bottomContainer: {
+    flex: 3,
+  },
+  button: {
+    flex: 1,
+
+    margin: 12,
+    height: 60,
+
+    borderRadius: 12,
+    backgroundColor: '#fff',
+    elevation: 2,
+    shadowOffset: {
+      width: 1,
+      height: 1,
+    },
+    shadowColor: '#333',
+    shadowOpacity: 0.1,
+    shadowRadius: 1,
+  },
+  selected: {
+    backgroundColor: '#ffeaa7',
+  },
 });
