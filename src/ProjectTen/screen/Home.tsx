d6e84cee45e2a58ca1bdfb89afe437a4bd5faba8
aman mulani <mulaniaman0504@gmail.com>
2023-12-31 15:50:17 +0530

    creating navigation screens

--- src/ProjectTen/screen/Home.tsx
+++ src/ProjectTen/screen/Home.tsx
@@ -1,14 +1,33 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
-const Home = () => {
+//react navigation
+
+import {NativeStackScreenProps} from '@react-navigation/native-stack';
+import {RootStackParamList} from '../App';
+
+import ProductItem from '../components/ProductItem';
+import Seperator from '../components/Seperator';
+import { PRODUCTS_LIST } from '../data/constant';
+
+type HomeProps = NativeStackScreenProps<RootStackParamList,"Home">
+const Home = ({navigation}:HomeProps) => {
   return (
-    <View>
+    <View style={styles.container}>
       <Text>Home</Text>
     </View>
   )
 }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'flex-start',
+    justifyContent: 'center',
+
+    padding: 12,
+    backgroundColor: '#FFFFFF',
+  },
+});
 
 export default Home
 
-const styles = StyleSheet.create({})
